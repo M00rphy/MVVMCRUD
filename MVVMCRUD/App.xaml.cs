5d9827ae0f7b0782130603e6dad81ba2e3286926
M00rphy <agpkkck@gmail.com>
2024-11-04 12:54:21 -0600

    Add project files.

---
+++ MVVMCRUD/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MVVMCRUD;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        WpfMVVMSample.MainWindow window = new MainWindow();
+        UserViewModel VM = new UserViewModel();
+        window.DataContext = VM;
+        window.Show();
+    }
+}
