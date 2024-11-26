44ec50ac8915cc15aae4f09ff3124d788bee8d6d
M00rphy <agpkkck@gmail.com>
2024-11-26 15:38:15 -0600

    add new

--- UsersCRUD/View/UserControlWindow.xaml.cs
+++ UsersCRUD/View/UserControlWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Windows;
+using UsersCRUD.Model;
 
 namespace UsersCRUD
 {
@@ -14,6 +15,20 @@ namespace UsersCRUD
         {
             InitializeComponent();
             this.DataContext = new UserViewModel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var newUser = new User
+            {
+                Name = NameIn.Text,
+                LastName = LastNameIn.Text,
+                Email = EmailIn.Text,
+                Password = PasswordIn.Text
+            };
+
+            var viewModel = DataContext as UserViewModel;
+            viewModel?.Users.Add(newUser);
         }
     }
 }
